087b1d9c57c61b3f258746b19026067cb4b34939
Roman Demissie <romimulye@gmail>
2022-10-18 16:23:14 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main -print a line using puts
+ * 
+ * Retrun: 0
+*/
+int main (void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	retrun (0);
+}	
