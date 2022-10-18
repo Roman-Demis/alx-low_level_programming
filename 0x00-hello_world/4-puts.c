359dadf1d19fb18dc7cc936284f69c71c5b2cace
Roman Demissie <romimulye@gmail>
2022-10-18 16:42:39 -0700

    myfirst c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
- * main -print a line using puts
- * 
+/**
+ * main -Print a line of code using puts
+ *
  * Retrun: 0
 */
+
 int main (void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	retrun (0);
-}	
+	retrun(0);
+}
